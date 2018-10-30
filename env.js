243c671426e959a5514aaae0c04c6073064f0efb
liyuan <liyuan900226@163.com>
2018-10-30 14:19:00 +0800

    liyuan

---
+++ env.js
@@ -0,0 +1,4 @@
+var DOMAIN_API_CONFIG_SYS="https://mapiconfig-dev.m.xueceping.cn";
+//var DOMAIN_API_CONFIG_SYS="https://mapiconfig.m.tbkt.cn";
+//var DOMAIN_API_CONFIG_SYS="http://192.168.7.117:7000";
+//var ROOT_URL="config-dev.m.xueceping.cn";
